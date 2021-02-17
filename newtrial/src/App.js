172a9d93c15c5971a86dfcea68b2927a028a7858
TharikaVijayangi <it18016208@my.sliit.lk>
2021-02-17 21:24:27 +0530

    Add react bootsrap UI

--- newtrial/src/App.js
+++ newtrial/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <h1>Hellooo</h1>
+      <Button variant="primary">Primary</Button>{' '}
+  <Button variant="secondary">Secondary</Button>{' '}
+  <Button variant="success">Success</Button>{' '}
+  <Button variant="warning">Warning</Button>{' '}
     </div>
   );
 }
